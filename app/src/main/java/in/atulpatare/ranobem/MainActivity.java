3a15bb139185db0d534f2ce1ed44efa4e8272d71
atul <atulpatare99@gmail.com>
2025-07-09 19:32:24 +0530

    download pro button added

--- app/src/main/java/in/atulpatare/ranobem/MainActivity.java
+++ app/src/main/java/in/atulpatare/ranobem/MainActivity.java
@@ -3,6 +3,7 @@ package in.atulpatare.ranobem;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +11,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import in.atulpatare.ranobem.config.Config;
 import in.atulpatare.ranobem.databinding.ActivityMainBinding;
 import in.atulpatare.ranobem.ui.browse.BrowseActivity;
 
@@ -28,10 +30,15 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        if (Config.isFree()) {
+            binding.downloadProContainer.setVisibility(View.VISIBLE);
+        }
+
         binding.browse.setOnClickListener(v -> startActivity(new Intent(this, BrowseActivity.class)));
         binding.discord.setOnClickListener(v -> navigateToLink("https://discord.gg/6CQ6u64dca"));
         binding.x.setOnClickListener(v -> navigateToLink("https://x.com/atul_patare"));
         binding.lightnovels.setOnClickListener(v -> navigateToLink("https://play.google.com/store/apps/details?id=org.ranobe.downloader"));
+        binding.downloadPro.setOnClickListener(v -> navigateToLink("https://play.google.com/store/apps/details?id=in.atulpatare.ranobem.pro"));
     }
 
     private void navigateToLink(String url) {
