69ea70d38d2021dd09291aa873af4021c92a5303
atul <atulpatare99@gmail.com>
2025-11-05 22:26:17 +0530

    reenable pro and free version

--- app/src/main/java/in/atulpatare/ranobem/config/Config.java
+++ app/src/main/java/in/atulpatare/ranobem/config/Config.java
@@ -1,11 +1,13 @@
 package in.atulpatare.ranobem.config;
 
+import in.atulpatare.ranobem.BuildConfig;
+
 public class Config {
     public static final String KEY_MANGA = "manga";
     public static final String KEY_CHAPTER = "chapter";
     public static final String KEY_CHAPTER_LIST = "chapter_list";
 
     public static boolean isFree() {
-        return false;
+        return !BuildConfig.IS_PRO;
     }
 }
